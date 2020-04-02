0977c7f3c3f97ccbd70a50743be35d096761b808
shalini-techindustan <shalini@techindustan.com>
2020-04-02 16:17:29 +0530

    Worked on agGrid Table

---
+++ src/app/agGrid/agGridTable.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+
+class AgGridTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tableData: {},
+            defaultColDef: {
+                sortable: this.props.sortable,
+                filter: this.props.filter,
+                resizable: this.props.resizable,
+            },
+            defaultGridOptions: {
+                rowDragManaged: this.props.rowDragManaged,
+                animateRows: true,
+                rowSelection : 'multiple',
+                onSelectionChanged: this.onSelectionChanged
+            }
+        }
+    }
+
+
+    // Function Name : onSelectionChanged
+    // Parameters : params
+    //Functionality : We can find here the selected rows.
+
+    onSelectionChanged = (params) => {
+        // All Seleted Rows Data are here
+        const selected_rows = params && params.api && params.api.getSelectedRows();
+    }
+
+    // Function Name : onGridReady
+    // Parameters : params
+    //Functionality : Run initially when agGrid is mounted on DOM. 
+    onGridReady = (params) => {
+        this.gridApi = params.api;
+    }
+
+    // Function Name : onFirstDataRendered
+    // Parameters : params
+    //Functionality : When data is initially loaded on table. This function call sizeColumsToFit ==> Adjust the column size to full width of table . 
+
+    onFirstDataRendered = (params) => {
+        params.api.sizeColumnsToFit();
+    }
+
+    render() {
+        const { tableData, rowHeight = 100, dataRenderWidth = true } = this.props;
+
+        return (
+            <div className="ag-theme-material jobs-container" style={{ height: '700px', width: '100%' }}>
+                <AgGridReact
+                    columnDefs={tableData.columnDefs}
+                    rowData={tableData.rowData}
+                    //   components={tableData.components}
+                    //   frameworkComponents={tableData.frameworkComponents}
+                    rowHeight={rowHeight}
+                    defaultColDef={this.state.defaultColDef}
+                    onFirstDataRendered={dataRenderWidth ? this.onFirstDataRendered : () => { }}
+                    gridOptions={this.state.defaultGridOptions}
+                >
+                </AgGridReact>
+            </div>
+        );
+    }
+}
+
+export default AgGridTable;
--- src/app/main/containers/installDevices/DevicesTable.js
+++ src/app/main/containers/installDevices/DevicesTable.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import AgGridTable from "../../../agGrid/agGridTable";
 import { withStyles } from '@material-ui/core/styles';
 import { Table, TableBody, TableCell, TablePagination, TableRow, Checkbox } from '@material-ui/core';
 import { FuseScrollbars, FuseUtils } from '@fuse';
@@ -10,14 +11,70 @@ const styles = theme => ({
     layoutRoot: {}
 });
 
+
+
 class InstalledDevicesTable extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            tableData: {}
+        }
 
     }
+
+
+    componentDidMount = () => {
+        fetch("https://pomber.github.io/covid19/timeseries.json")
+            .then(response => response.json())
+            .then(data => {
+                this.createTableData(data);
+            })
+    }
+
+    // This is for temporary use until we have API
+    // Delete it when we have API's for implementation.
+    createTableData = (data) => {
+        let corona_data = data['India'];
+        let tableData = {
+            columnDefs: [
+                {
+                    headerName: "Date", field: "date", rowDrag: true,
+                    checkboxSelection: () => {
+                        console.log("Rinnonh=g")
+                        return true;
+                    }
+                },
+                { headerName: "Confirmed", field: "confirmed" },
+                { headerName: "Death", field: "deaths" },
+                { headerName: "Recovered", field: "recovered" }
+            ],
+            rowData: corona_data && corona_data.length && corona_data.map((item, index) => {
+                return {
+                    date: item.date,
+                    confirmed: item.confirmed || 0,
+                    deaths: item.deaths || 0,
+                    recovered: item.recovered
+                }
+            })
+        }
+        this.setState({ tableData });
+    }
+
     render() {
+        const {
+            tableData
+        } = this.state;
+
         return (
-            <div>InstalledDevicesTable</div>
+            <div>
+                <AgGridTable
+                    tableData={tableData}
+                    resizable={true}
+                    filter={true}
+                    sortable={true}
+                    rowDragManaged={true}
+                />
+            </div>
         )
     }
 }
