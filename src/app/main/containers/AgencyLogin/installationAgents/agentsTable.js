7ee8a76e480087f9bd0d30458a6c816aadd00976
shalini-techindustan <shalini@techindustan.com>
2020-05-07 20:10:14 +0530

    add show/hide password in agent table

--- src/app/main/containers/AgencyLogin/installationAgents/agentsTable.js
+++ src/app/main/containers/AgencyLogin/installationAgents/agentsTable.js
@@ -7,35 +7,57 @@ import _ from '@lodash';
 import { Link } from 'react-router-dom';
 import { CustomPagination } from "../../../components";
 import { FuseAnimate } from '@fuse';
-import { Button } from '@material-ui/core';
-import {data} from '../agents.json';
+import { Button, Icon } from '@material-ui/core';
+import { data } from '../agents.json';
 
 const styles = theme => ({
     layoutRoot: {}
 });
 
-
-
+var showPassword = false
 class AgentsTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tableData: {}
+            tableData: {},
+            showPassword: false
         }
 
     }
 
+    onGridReady = (params) => {
+        this.gridApi = params.api;
+    }
+
+    // Function name : handleClickShowPassword or update rowDataItems
+    // Parameters : id, item_index
+    // Functionality :handleChange  It is user to only update the one row not the entire table render again.
+    handleClickShowPassword = (item_index) => {
+        var itemsToUpdate = [];
+        this.gridApi.forEachNodeAfterFilterAndSort(function (rowNode, index) {
+            if (item_index != rowNode.id) {
+                return;
+            } else {
+                var data = rowNode.data;
+                data.showPassword = !data.showPassword;
+                data.password = data.password;
+                itemsToUpdate.push(data)
+            }
+        });
+        var res = this.gridApi.updateRowData({ update: itemsToUpdate });
+    };
 
     componentDidMount = () => {
         this.createTableData(data);
     }
 
     createTableData = (data) => {
-        const agentsData  = data;
+        const agentsData = data;
         let tableData = {
             frameworkComponents: {
                 actionButtonRender: ActionButtonRender,
-                actionStatusRender: ActionStatusRender
+                actionStatusRender: ActionStatusRender,
+                actionPasswordRender: ActionPasswordRender
             },
             columnDefs: [
                 {
@@ -43,18 +65,22 @@ class AgentsTable extends Component {
                 },
                 { headerName: "Email ID", field: "email" },
                 { headerName: "Mobile No.", field: "mobile" },
-                { headerName: "Password", field: "password" },
-                { headerName: "Status", field: "status", cellRenderer:'actionStatusRender' },
+                { headerName: "Password", field: "showPassword", cellRenderer: 'actionPasswordRender' },
+                { headerName: "Status", field: "status", cellRenderer: 'actionStatusRender' },
                 { headerName: "Actions", field: "actions", cellRenderer: 'actionButtonRender' }
             ],
             rowData: agentsData && agentsData.length && agentsData.map((item, index) => {
                 return {
+                    // index:index,
                     name: item.name,
                     email: item.email || null,
                     mobile: item.mobile,
-                    password:item.password,
-                    status:item.status,
-                    editFunction: this.editFunction
+                    password: item.password,
+                    status: item.status,
+                    showPassword: false,
+                    rowData: item,
+                    editFunction: this.editFunction,
+                    handleClickShowPassword: this.handleClickShowPassword
                 }
             })
         }
@@ -83,7 +109,8 @@ class AgentsTable extends Component {
                         ...tableData,
                         frameworkComponents: {
                             actionButtonRender: ActionButtonRender,
-                            actionStatusRender: ActionStatusRender
+                            actionStatusRender: ActionStatusRender,
+                            actionPasswordRender: ActionPasswordRender
                         }
                     }}
                     resizable={true}
@@ -92,6 +119,7 @@ class AgentsTable extends Component {
                     rowDragManaged={true}
                     rowHeight={50}
                     height={'650px'}
+                    onGridReady={this.onGridReady}
                 />
                 <div className='flex flex-1 items-center justify-end'>
                     <CustomPagination />
@@ -121,7 +149,24 @@ function ActionButtonRender(item) {
 function ActionStatusRender(item) {
     return <span >
         {
-            item.value ==true ? <span style={{ color: 'green' }}>Active</span> : <span style={{ color: 'red' }}>Inactive</span>
+            item.value == true ? <span style={{ color: 'green' }}>Active</span> : <span style={{ color: 'red' }}>Inactive</span>
         }
     </span>
+}
+
+function ActionPasswordRender(item) {
+    const { data = {} } = item;
+    const { handleClickShowPassword, index } = data;
+    // const { id = "" } = rowData;
+    return <div className='flex items-center'>
+        {data.showPassword ?
+            <><span className='px-2'>{data.password}</span>
+                <Icon className='cursor-pointer' onClick={() => handleClickShowPassword(item.rowIndex)} >visibility</Icon>
+            </>
+            :
+            <><span className='iconPass px-2'>{data.password}</span>
+                <Icon className='cursor-pointer' onClick={() => handleClickShowPassword(item.rowIndex)}>visibility_off</Icon>
+            </>
+        }
+    </div>
 }
